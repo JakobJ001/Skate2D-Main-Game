c6d86e4bb0e04d47f4420aeba604924504b02dbc
jakob.jansson <jakob.jansson16@itgksd.se>
2018-05-03 11:19:11 +0200

    I did a lot of stuff

--- Skate2D Controls Test/Player.cs
+++ Skate2D Controls Test/Player.cs
@@ -22,8 +22,17 @@ namespace Skate2D_Controls_Test
     public class Player
     {
         public Texture2D texture;
-        public Vector2 position, velocity;
+        public Vector2 position, velocity, acceleration, friction, center;
         float rotation;
+        KeyboardState nowButton, lastButton;
+        int layer;
+        int ground;
+
+        float accelerationfloat = 0.1F;
+        float frictionfloat = 0;
+        float maxspeed = 20;
+        float startspeed = 0;
+        float restSpeed = 0;
 
         //Constructor, assigns the important variables to the player, like everything it needs to move/how it looks. Basically creates the player.
         public Player(Texture2D texture)
@@ -31,10 +40,18 @@ namespace Skate2D_Controls_Test
             this.texture = texture;
             position = new Vector2(50, 100);
             velocity = new Vector2(0, 0);
+            acceleration = new Vector2(0, 0);
+            friction = new Vector2(0, 0);
+            frictionfloat = accelerationfloat / 2;
+            restSpeed = accelerationfloat;
+
+            center = new Vector2(texture.Width / 2, texture.Height / 2);
         }
 
         public void Update()
         {
+            //
+            nowButton = Keyboard.GetState();
             //--- These are the two vektors related to the player, and is what changes if the player moves. 
             velocity += Game1.gravity;
             position += velocity;
@@ -47,18 +64,60 @@ namespace Skate2D_Controls_Test
                 JumpTime++;
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
-                velocity.X = -20;
-            }
+            // --- ARVIDS DEL ---
 
+            //så man kan styra höger/vänster, igenom att ändra på acceleration
             if (Keyboard.GetState().IsKeyDown(Keys.D))
-            { 
-                    velocity.X = 20;
-
+            {
+                acceleration.X = accelerationfloat;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                acceleration.X = -accelerationfloat;
+            }
+            //sätter acceleration till 0 om ingen knapp är nedtryckt
+            else
+            {
+                acceleration.X = 0;
             }
 
-            //--- S and D are assigned to rotate the character, so that in the final game you can do flips in the air for extra points.
+            //Spelaren får hastighet igenom att addera velocity med acceleration
+            velocity += acceleration;
+
+            //Om velocity råkar vara mindre än restspeed så stängs all hastighet och friktion av. Detta är för att inte få den att skaka när den står stil
+            if (System.Math.Abs(velocity.X) < restSpeed)
+            {
+                velocity.X = 0;
+                friction.X = 0;
+            }
+            else
+            {
+                //Skapar friktion mot spelaren. Helt enkelt kraft som alltid åker mot spelarens håll.
+                if (velocity.X > 0)
+                {
+                    friction.X = -frictionfloat;
+                }
+                else if (velocity.X < 0)
+                {
+                    friction.X = frictionfloat;
+                }
+            }
+
+            //Spelaren får friktion
+            velocity += friction;
+
+            //Om hastigheten råkar gå över maxhastigheten, så sätts den tillbaks till maxhastigheten. Detta fixar en massa buggar
+            if (velocity.X > maxspeed)
+            {
+                velocity.X = maxspeed;
+            }
+
+            //Spelaren rör på sig och ändrar position
+            position += velocity;
+
+            // --- END ---
+
+            //--- E and Q are assigned to rotate the character, so that in the final game you can do flips in the air for extra points.
             if (Keyboard.GetState().IsKeyDown(Keys.E))
             {
                     rotation += MathHelper.ToRadians(10);
@@ -68,6 +127,25 @@ namespace Skate2D_Controls_Test
             if (Keyboard.GetState().IsKeyDown(Keys.Q))
             {
                     rotation -= MathHelper.ToRadians(10);
+            }
+
+            //--- W and S are assigned to change the layer that the character is on so that he can avoid obstacles.
+            if (Keyboard.GetState().IsKeyDown(Keys.W) && layer != 3 && lastButton != nowButton)
+            {
+                layer += 1;
+                if (position.Y > 800 - texture.Height + 4 * 40)
+                {
+                    position.Y -= 800 - texture.Height + layer + 1 *40;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S) && layer != 0 && lastButton != nowButton)
+            {
+                layer -= 1;
+                if (position.Y > 800 - texture.Height + 4 * 40)
+                {
+                    position.Y = 800 - texture.Height - layer * 40 ;
+                }
             }
 
             //--- This is simply a key to return to rotation 0, but i didnt want it to just teleport into position so i used
@@ -100,11 +178,13 @@ namespace Skate2D_Controls_Test
 
 
             //--- This  simply creates the ground, so you dont fall forever
-            if (position.Y > 800 - texture.Height && velocity.Y > 1)
+            ground = 800 - texture.Height - layer * 40;
+
+            if (position.Y > ground && velocity.Y > 1)
             {
                 velocity.Y *= -1;
                 velocity.Y /= 2;
-                position.Y = 799 - texture.Height;
+                position.Y = ground - 1;
                 
                 //--- This makes it so that if youre rotated a certain way, in where the player textures wheels wouldnt hit the ground when you land,
                 //-- you stop and basically crash. Gives a bit more risk to rotating in the air.
@@ -114,6 +194,8 @@ namespace Skate2D_Controls_Test
                     rotation = 0;
                 }
             }
+
+            lastButton = nowButton;
 
         }
 
